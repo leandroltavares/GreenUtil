a0b7dcdb026b6447a55af5477cf3298c14c2d394
Leandro Luciani Tavares <leandroltavares@users.noreply.github.com>
2018-06-25 17:18:09 -0300

    Added IsSQLValidDateTime
    Added IsSQLValidDateTime

---
+++ GreenUtil.Test/Data/DateTimeUtilTest.cs
@@ -0,0 +1,51 @@
+﻿using GreenUtil.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenUtil.Test.Data
+{
+    [TestClass]
+    public class DateTimeUtilTest
+    {
+        [TestMethod]
+        public void WhenDateTimeIsMinDateThenIsValidSQLDateTimeShouldReturnFalse()
+        {
+            Assert.IsFalse(DateTimeUtil.IsValidSQLDateTime(DateTime.MinValue));
+        }
+
+        [TestMethod]
+        public void WhenDateTimeIsLessThanSqlMinValueThenIsValidSQLDateTimeShouldReturnFalse()
+        {
+            Assert.IsFalse(DateTimeUtil.IsValidSQLDateTime(SqlDateTime.MinValue.Value.AddTicks(-1)));
+        }
+
+        [TestMethod]
+        public void WhenDateTimeIsEqualThanSqlMinValueThenIsValidSQLDateTimeShouldReturnTrue()
+        {
+            Assert.IsTrue(DateTimeUtil.IsValidSQLDateTime(SqlDateTime.MinValue.Value));
+        }
+
+        [TestMethod]
+        public void WhenDateTimeIsMaxDateThenIsValidSQLDateTimeShouldReturnFalse()
+        {
+            Assert.IsFalse(DateTimeUtil.IsValidSQLDateTime(DateTime.MaxValue));
+        }
+
+        [TestMethod]
+        public void WhenDateTimeIsGreaterThanSqlMaxValueThenIsValidSQLDateTimeShouldReturnFalse()
+        {
+            Assert.IsFalse(DateTimeUtil.IsValidSQLDateTime(SqlDateTime.MaxValue.Value.AddTicks(1)));
+        }
+
+        [TestMethod]
+        public void WhenDateTimeIsEqualThanSqlMaxValueThenIsValidSQLDateTimeShouldReturnTrue()
+        {
+            Assert.IsTrue(DateTimeUtil.IsValidSQLDateTime(SqlDateTime.MaxValue.Value));
+        }
+    }
+}
---
+++ GreenUtil/Data/DateTimeUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenUtil.Data
+{
+    /// <summary>
+    /// Logic relate do DateTime
+    /// </summary>
+    public static class DateTimeUtil
+    {
+        /// <summary>
+        /// Determines whether a DateTime is a valid SQL DateTime
+        /// </summary>
+        /// <param name="date">The date to be evaluated</param>
+        /// <returns>True if valid SQL DateTime, false otherwise</returns>
+        public static bool IsValidSQLDateTime(this DateTime date)
+        {
+            return date >= SqlDateTime.MinValue.Value && date <= SqlDateTime.MaxValue.Value;
+        }
+    }
+}
