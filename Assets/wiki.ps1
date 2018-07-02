744314303bed023b86858a95f95c7d2f7cce89b2
Leandro Luciani Tavares <leandro.ltavares@gmail.com>
2018-07-02 17:38:56 -0300

    fix yml
    fix yml

--- Assets/wiki.ps1
+++ Assets/wiki.ps1
@@ -1,16 +1,16 @@
 Param([string]$token)
 
-$mdFolder = C:\projects\wiki2
+$mdFolder = "C:\projects\wiki2"
 
 Write-Host "Start publishing documentation to wiki"
 git config --global credential.helper store
-Add-Content "$HOME\.git-credentials" "https://$($token):x-oauth-basic@github.com`n"
+Add-Content "$HOME\.git-credentials" "https://$($token):x-oauth-basic@github.com"
 git config --global user.email "leandro.ltavares@gmail.com"
 git config --global user.name "Leandro Luciani Tavares"
 Remove-Item $mdFolder -Force -Recurse -ErrorAction SilentlyContinue
 md $mdFolder
 cd $mdFolder
-git clone --branch=master https://github.com/leandroltavares/GreenUtil.wiki.git 
+git clone --branch=master "https://github.com/leandroltavares/GreenUtil.wiki.git" -q
 cd GreenUtil.wiki
 git status
 C:\projects\greenutil\Assets\MarkdownGenerator.exe "C:\projects\greenutil\GreenUtil\bin\Release\net471\GreenUtil.dll" "$($mdFolder)\GreenUtil.wiki"
@@ -19,6 +19,6 @@ git add .
 git status
 git commit -m "Commiting wiki changes"
 git status
-git push
+git push -q
 git status
 Write-Host "Start publishing documentation to wiki"
