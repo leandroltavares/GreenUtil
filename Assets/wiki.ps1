a8aacb52407606c6030e4fa9f157a47478bd7c4b
Leandro Luciani Tavares <leandro.ltavares@gmail.com>
2018-07-04 21:40:50 -0300

    changed token attempt
    changed token attempt

--- Assets/wiki.ps1
+++ Assets/wiki.ps1
@@ -10,10 +10,10 @@ git config --global user.name "Leandro Luciani Tavares"
 Remove-Item $mdFolder -Force -Recurse -ErrorAction SilentlyContinue
 md $mdFolder
 cd $mdFolder
-#git clone --branch=master "https://github.com/leandroltavares/GreenUtil.wiki.git" -q
-#cd GreenUtil.wiki
-git clone --branch=master "https://github.com/leandroltavares/netdeveloperpraticum.wiki.git" -q
-cd netdeveloperpraticum.wiki
+git clone --branch=master "https://github.com/leandroltavares/GreenUtil.wiki.git" -q
+cd GreenUtil.wiki
+#git clone --branch=master "https://github.com/leandroltavares/netdeveloperpraticum.wiki.git" -q
+#cd netdeveloperpraticum.wiki
 git status
 #C:\projects\greenutil\Assets\MarkdownGenerator.exe "C:\projects\greenutil\GreenUtil\bin\Release\net471\GreenUtil.dll" "$($mdFolder)\GreenUtil.wiki"
 #git status
